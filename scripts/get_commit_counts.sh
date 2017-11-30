836bbcf1a8f586b6940f78d5f115bb1cabb46c34
Tracy Kuhrt <tkuhrt@linuxfoundation.org>
2017-11-30 13:52:54 -0700

    Allow specification of project

--- scripts/get_commit_counts.sh
+++ scripts/get_commit_counts.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -x
 
 script_dir=`dirname $0`
 
@@ -10,10 +11,11 @@ fi
 . `dirname $0`/repositories.sh
 
 #Default variables
-repositories="${all_repositories[@]}" 
+repositories=()
 filename="hyperledger-commit-counts"
 since=""
 until=""
+all_specified=FALSE
 
 # Handle command line arguments
 while [[ $# -gt 0 ]]
@@ -21,6 +23,71 @@ do
 key="$1"
 
 case $key in
+    --fabric)
+      if [[ "$all_specified" == FALSE ]] ; then
+        repositories+=( "${fabric_repositories[@]}" )
+        filename+="-fabric"
+      fi
+    ;;
+    --sawtooth)
+      if [[ "$all_specified" == FALSE ]] ; then
+        repositories+=( "${sawtooth_repositories[@]}" )
+        filename+="-sawtooth"
+      fi
+    ;;
+    --iroha)
+      if [[ "$all_specified" == FALSE ]] ; then
+        repositories+=( "${iroha_repositories[@]}" )
+        filename+="-iroha"
+      fi
+    ;;
+    --burrow)
+      if [[ "$all_specified" == FALSE ]] ; then
+        repositories+=( "${burrow_repositories[@]}" )
+        filename+="-burrow"
+      fi
+    ;;
+    --indy)
+      if [[ "$all_specified" == FALSE ]] ; then
+        repositories+=( "${indy_repositories[@]}" )
+        filename+="-indy"
+      fi
+    ;;
+    --composer)
+      if [[ "$all_specified" == FALSE ]] ; then
+        repositories+=( "${composer_repositories[@]}" )
+        filename+="-composer"
+      fi
+    ;;
+    --cello)
+      if [[ "$all_specified" == FALSE ]] ; then
+        repositories+=( "${cello_repositories[@]}" )
+        filename+="-cello"
+      fi
+    ;;
+    --explorer)
+      if [[ "$all_specified" == FALSE ]] ; then
+        repositories+=( "${explorer_repositories[@]}" )
+        filename+="-explorer"
+      fi
+    ;;
+    --gerrit)
+      if [[ "$all_specified" == FALSE ]] ; then
+        repositories+=( "${gerrit_repositories[@]}" )
+        filename+="-gerrit"
+      fi
+    ;;
+    --github)
+      if [[ "$all_specified" == FALSE ]] ; then
+        repositories+=( "${github_repositories[@]}" )
+        filename+="-github"
+      fi
+    ;;
+    --all)
+      all_specified=TRUE
+      filename+="-all"
+      repositories="${all_repositories[@]}"
+    ;;
     --since)
       since="$2"
       shift # past argument or value. 2nd shift below
@@ -32,14 +99,25 @@ case $key in
     --help)
       cat << EOM
         get_commit-counts.sh [--since mm/dd/yyyy] [--until mm/dd/yyyy]
-        Get commit count from all Hyperledger repositories.
+        Get commit count from Hyperledger repositories.
 
         Options:
-          --since: Specify since which date to obtain contributors (mm/dd/yyyy).
-                   By default obtains contributors from the start of the repo.
-          --until: Specify until which date to obtain contributors (mm/dd/yyyy).
-                   By default obtains contributors until the end of commits.
-          --help:  Shows this help message
+          --fabric:   Include Fabric repositories
+          --sawtooth: Include Sawtooth repositories
+          --iroha:    Include Iroha repositories
+          --burrow:   Include Burrow repositories
+          --indy:     Include Indy repositories
+          --composer: Include Composer repositories
+          --cello:    Include Cello repositories
+          --explorer: Include Explorer repositories
+          --gerrit:   Include Gerrit repositories
+          --github:   Include Github repositories
+          --all:      Include all repositories (default)
+          --since:    Since which date to obtain contributors (mm/dd/yyyy).
+                      By default obtains contributors from start of the repo.
+          --until:    Until which date to obtain contributors (mm/dd/yyyy).
+                      By default obtains contributors until the end of commits.
+          --help:     Shows this help message
 EOM
     exit;
     ;;
@@ -50,6 +128,13 @@ EOM
 esac
 shift # past argument or value
 done
+
+# if no repositories were specified, then act as if --all was specified
+if [ "$repositories" == "" ]
+then
+  repositories="${all_repositories[@]}"
+  filename+="-all"
+fi
 
 today=`date -u +%Y-%m-%d-%H-%M-%S`
 outdir=/tmp/${filename}-${today}
