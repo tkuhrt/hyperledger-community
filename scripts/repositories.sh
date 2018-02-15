c54c6cb09c59e981edecffa543f03403836a0adf
Tracy Kuhrt <tkuhrt@linuxfoundation.org>
2018-02-15 16:35:49 -0700

    Updated list of repos
    Signed-off-by: Tracy Kuhrt <tkuhrt@linuxfoundation.org>

--- scripts/repositories.sh
+++ scripts/repositories.sh
@@ -4,6 +4,9 @@ fabric_repositories=(
   https://gerrit.hyperledger.org/r/fabric
   https://gerrit.hyperledger.org/r/fabric-baseimage
   https://gerrit.hyperledger.org/r/fabric-ca
+  https://gerrit.hyperledger.org/r/fabric-chaincode-evm
+  https://gerrit.hyperledger.org/r/fabric-chaincode-java
+  https://gerrit.hyperledger.org/r/fabric-chaincode-node
   https://gerrit.hyperledger.org/r/fabric-chaintool
   https://gerrit.hyperledger.org/r/fabric-docs
   https://gerrit.hyperledger.org/r/fabric-samples
@@ -11,29 +14,33 @@ fabric_repositories=(
   https://gerrit.hyperledger.org/r/fabric-sdk-java
   https://gerrit.hyperledger.org/r/fabric-sdk-node
   https://gerrit.hyperledger.org/r/fabric-sdk-py
+  https://gerrit.hyperledger.org/r/fabric-sdk-rest
+  https://gerrit.hyperledger.org/r/fabric-test
   https://gerrit.hyperledger.org/r/fabric-test-resources
   https://gerrit.hyperledger.org/r/homebrew-fabric
-  https://gerrit.hyperledger.org/r/fabric-test
-  https://gerrit.hyperledger.org/r/fabric-chaincode-node
-  https://gerrit.hyperledger.org/r/fabric-sdk-rest
-  https://gerrit.hyperledger.org/r/fabric-chaincode-java
 )
 
 sawtooth_repositories=(
   https://github.com/hyperledger/sawtooth-core.git
   https://github.com/hyperledger/sawtooth-supply-chain.git
+  https://github.com/hyperledger/sawtooth-seth.git
+  https://github.com/hyperledger/sawtooth-marketplace.git
+  https://github.com/hyperledger/sawtooth-hyper-directory.git
+  https://github.com/hyperledger/sawtooth-explorer.git
+  https://github.com/hyperledger/sawtooth-private-utxo.git
 )
 
 iroha_repositories=(
   https://github.com/hyperledger/iroha.git
-  https://github.com/hyperledger/iroha-javascript.git
-  https://github.com/hyperledger/iroha-ametsuchi.git
-  https://github.com/hyperledger/iroha-scala.git
-  https://github.com/hyperledger/iroha-ios.git
   https://github.com/hyperledger/iroha-android.git
-  https://github.com/hyperledger/iroha-network-tools.git
+  https://github.com/hyperledger/iroha-ios.git
   https://github.com/hyperledger/iroha-python.git
+  https://github.com/hyperledger/iroha-api.git
+  https://github.com/hyperledger/iroha-javascript.git
+  https://github.com/hyperledger/iroha-scala.git
   https://github.com/hyperledger/iroha-dotnet.git
+  https://github.com/hyperledger/iroha-network-tools.git
+  https://github.com/hyperledger/iroha-ed25519.git
 )
 
 burrow_repositories=(
@@ -41,8 +48,8 @@ burrow_repositories=(
 )
 
 indy_repositories=(
-  https://github.com/hyperledger/indy-plenum.git
   https://github.com/hyperledger/indy-node.git
+  https://github.com/hyperledger/indy-plenum.git
   https://github.com/hyperledger/indy-sdk.git
   https://github.com/hyperledger/indy-anoncreds.git
   https://github.com/hyperledger/indy-crypto.git
@@ -51,11 +58,12 @@ indy_repositories=(
 composer_repositories=(
   https://github.com/hyperledger/composer.git
   https://github.com/hyperledger/composer-sample-networks.git
+  https://github.com/hyperledger/composer-tools.git
   https://github.com/hyperledger/composer-sample-models.git
   https://github.com/hyperledger/composer-vscode-plugin.git
   https://github.com/hyperledger/composer-atom-plugin.git
   https://github.com/hyperledger/composer-sample-applications.git
-  https://github.com/hyperledger/composer-tools.git
+  https://github.com/hyperledger/composer-knowledge-wiki.git
 )
 
 cello_repositories=(
